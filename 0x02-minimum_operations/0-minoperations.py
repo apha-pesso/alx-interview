29f81ffb07c56ec7ec0d4ce19c03ce9ed5d3e67e
apha-pesso <mutiuayo99@gmail.com>
2023-04-05 12:30:11 +0100

    Minimum operations to get n characters

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Minimum Operations"""
+
+
+def isPrime(n):
+    """Check for prime numbers"""
+    for i in range(2, (n//2)):
+        if n % i == 0:
+            return False
+    return True
+
+
+def prime_factors(n):
+    """Return primefactors of a number"""
+    factors = []
+    for i in range(2, n//2):
+        while (n % i == 0):
+            factors.append(i)
+            n = n // i
+    if n > 1:
+        factors.append(n)
+    return factors
+
+
+def minOperations(n):
+    """Return minimum operations required to get n number of char"""
+    if isPrime(n):
+        return n
+    sum = 0
+    for i in prime_factors(n):
+        sum += i
+    return sum
