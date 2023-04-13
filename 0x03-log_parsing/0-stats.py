0136d0c77f56d3dcb4b71b21deba01ff4bf1d7bc
apha-pesso <mutiuayo99@gmail.com>
2023-04-13 18:24:25 +0100

    Raise error after printing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,29 +1,32 @@
 #!/usr/bin/python3
 """Log parsing"""
 
-import sys
+from sys import stdin
 
 total_file_size = 0
 counter = 0
 status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 
-
 try:
-    for line in sys.stdin:
+    for line in stdin:
         files = line.split()
+
         if int(files[-2]) in status_codes.keys() and len(files) == 9:
             code = int(files[-2])
             counter += 1
             status_codes[code] += 1
             total_file_size += int(files[-1])
+
         if counter % 10 == 0:
             print('File size: {}'.format(total_file_size))
             sorted_codes = dict(sorted(status_codes.items()))
             for key, value in sorted_codes.items():
                 if value:
                     print("{}: {}".format(key, value))
-except KeyboardInterrupt as error:
+
+except (KeyboardInterrupt):
     print('File size: {}'.format(total_file_size))
     sorted_codes = dict(sorted(status_codes.items()))
     for key, value in sorted_codes.items():
         print("{}: {}".format(key, value))
+    raise
