246c981197d661c16543d77b28474c8204e3c602
apha-pesso <mutiuayo99@gmail.com>
2023-04-15 14:37:37 +0100

    Checks for line validity

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -9,14 +9,16 @@ status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 try:
     for line in stdin:
         files = line.split()
+        if (len(files) < 2):
+            pass
 
         if int(files[-2]) in status_codes.keys() and len(files) == 9:
             code = int(files[-2])
             counter += 1
             status_codes[code] += 1
             total_file_size += int(files[-1])
-        else:
-            pass
+        # else:
+            # pass
 
         if counter % 10 == 0:
             print("File size: " + str(total_file_size))
