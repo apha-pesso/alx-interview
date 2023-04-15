e2174fd3bfc922331080564abc7f9c40add20190
apha-pesso <mutiuayo99@gmail.com>
2023-04-15 15:18:02 +0100

    Add extra check

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -9,24 +9,29 @@ status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 try:
     for line in stdin:
         files = line.split()
+        print("line {} -> {}".format(counter, files))
         if (len(files) < 2):
-            pass
+            continue
+        # if int(files[-2]) in status_codes.keys() and len(files) == 9:
 
-        if int(files[-2]) in status_codes.keys() and len(files) == 9:
+        try:
+
+            # if int(files[-2]) in status_codes.keys():
             code = int(files[-2])
             counter += 1
             status_codes[code] += 1
             total_file_size += int(files[-1])
-        # else:
-            # pass
+            print("TOtal size in loop {}".format(total_file_size))
 
-        if counter % 10 == 0:
-            print("File size: " + str(total_file_size))
-            # print('File size: {}'.format(total_file_size))
-            sorted_codes = dict(sorted(status_codes.items()))
-            for key in sorted_codes.keys():
-                if sorted_codes[key]:
-                    print(str(key) + ": " + str(sorted_codes[key]))
+            if counter % 10 == 0:
+                print("File size: " + str(total_file_size))
+                sorted_codes = dict(sorted(status_codes.items()))
+                for key in sorted_codes.keys():
+                    if sorted_codes[key]:
+                        print(str(key) + ": " + str(sorted_codes[key]))
+        except ValueError:
+            continue
+
     print("File size: " + str(total_file_size))
     for key in status_codes.keys():
         if status_codes[key]:
