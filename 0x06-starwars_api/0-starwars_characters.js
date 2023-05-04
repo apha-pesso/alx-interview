b52d6fd7f68f0d2a830364ab2d50d56c319727c8
apha-pesso <mutiuayo99@gmail.com>
2023-05-05 00:56:46 +0100

    Print in order

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
+// Star wars api
 
 const request = require('request');
 
@@ -6,31 +7,28 @@ const id = process.argv[2];
 
 const base = 'https://swapi-api.alx-tools.com/api/';
 const fullUrl = `${base}films/${id}`;
-// response = request.get(fullUrl)
-// console.log(response.data)
 
 request(fullUrl, { json: true }, (error, response, body) => {
   if (error) {
     console.error(error);
   } else {
-    // console.log(body)
-    //  console.log("____----____")
-    // console.log(JSON.parse(body).characters)
-    // console.log(body.characters)
-    // console.log("____----____")
-    // console.log("____----____")
     const characters = body.characters;
-    for (const ch of characters) {
-      const chid = ch.split('/')[5];
-      //  console.log(ch_id);
-
-      const charUrl = `${base}people/${chid}`;
-      request(charUrl, { json: true }, (error, response, body) => {
-        if (error) { console.log(error); } else {
-          console.log(body.name);
-        }
-      });
-    }
-    // console.log(body.character);
+    printCharacters(characters, 0);
   }
 });
+
+function printCharacters (characters, index) {
+  if (index >= characters.length) {
+    return;
+  }
+  const chid = characters[index].split('/')[5];
+  const charUrl = `${base}people/${chid}`;
+  request(charUrl, { json: true }, (error, response, body) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(body.name);
+      printCharacters(characters, index + 1);
+    }
+  });
+}
