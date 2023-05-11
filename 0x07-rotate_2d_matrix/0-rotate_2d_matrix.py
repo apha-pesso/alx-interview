9bfba8f0c09c19e2f8ed1baa4415dc6ef28c2efc
apha-pesso <mutiuayo99@gmail.com>
2023-05-11 23:36:11 +0100

    Fix Pycodestyle

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -4,14 +4,13 @@
 
 def rotate_2d_matrix(matrix):
     '''Rotate 2d matrix'''
-    l = len(matrix)
+    mat_len = len(matrix)
 
     # Transpose matrix
-    for i in range(l):
-        for j in range(i, l):
+    for i in range(mat_len):
+        for j in range(i, mat_len):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
     # Reverse each of the rows
-    for i in range(l):
+    for i in range(mat_len):
         matrix[i] = matrix[i][::-1]
-
