832fd2d0033624200786d23e68743848c484699d
apha-pesso <mutiuayo99@gmail.com>
2023-05-24 11:08:30 +0100

    Check output

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -3,21 +3,24 @@
 
 
 def island_perimeter(grid):
-    '''Island perimeter'''
+    '''Calculate perimeter'''
     perimeter = 0
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            if grid[i][j] == 1:
-                if i > 0 and i < (len(grid) - 1):
-                    if grid[i - 1][j] == 0:
-                        perimeter += 1
+    for row_idx, row in enumerate(grid):
+        for col_idx, col in enumerate(row):
+            if col == 1:
+                perimeter += 4
 
-                    if grid[i + 1][j] == 0:
-                        perimeter += 1
+                if row_idx > 0 and row_idx < (len(grid) - 1):
+                    if grid[row_idx - 1][col_idx] == 1:
+                        perimeter -= 1
 
-                    if grid[i][j - 1] == 0:
-                        perimeter += 1
+                    if grid[row_idx + 1][col_idx] == 1:
+                        perimeter -= 1
 
-                    if grid[i][j + 1] == 0:
-                        perimeter += 1
+                    if row[col_idx + 1] == 1:
+                        perimeter -= 1
+
+                    if row[col_idx - 1] == 1:
+                        perimeter -= 1
+
     return perimeter
